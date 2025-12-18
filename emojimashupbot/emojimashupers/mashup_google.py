855788618bb3b23c8dd4960652b16f79f472a124
David-Lor <17401854+David-Lor@users.noreply.github.com>
2025-12-18 21:06:37 +0100

    feat(wip): parallel requests

--- emojimashupbot/emojimashupers/mashup_google.py
+++ emojimashupbot/emojimashupers/mashup_google.py
@@ -3,7 +3,7 @@ import httpx
 from .mashup_interface import MashupInterface
 from ..models import Emoji, EmojiMashupResultComplete
 from ..settings import MashupersSettings
-from ..utils import Singleton
+from ..utils import Singleton, AsyncPool
 
 
 class GoogleMashup(MashupInterface, Singleton):
@@ -26,16 +26,29 @@ class GoogleMashup(MashupInterface, Singleton):
                 url = f"https://www.gstatic.com/android/keyboard/emojikitchen/{revision}/{unicode1}/{unicode1}_{unicode2}.png"
                 urls.append(url)
 
-        async with httpx.AsyncClient() as client:
-            random.shuffle(urls)
-            for url in urls:
-                    response = await client.get(url)
-                    print(url, response.status_code)
-                    if response.status_code == 200:
-                        return EmojiMashupResultComplete(
-                            emojis=[emoji1, emoji2],
-                            result_url=url,
-                            result_data=response.content,
-                        )
+        random.shuffle(urls)
+        runner = AsyncPool[httpx.Response](concurrency_limit=5)
 
+        async with httpx.AsyncClient() as client:
+            for url in urls:
+                runner.add_task(self._request_mashup(client, url, runner))
+            await runner.run()
+
+        if responses := [r for r in runner.results if r]:
+            response = responses[0]
+            return EmojiMashupResultComplete(
+                emojis=[emoji1, emoji2],
+                result_url=str(response.url),
+                result_data=response.content,
+            )
+
+        return None
+
+    @staticmethod
+    async def _request_mashup(client: httpx.AsyncClient, url: str, pool: AsyncPool) -> httpx.Response | None:
+        response = await client.get(url)
+        print(url, response.status_code)
+        if response.status_code == 200:
+            pool.stop = True
+            return response
         return None
--- emojimashupbot/utils.py
+++ emojimashupbot/utils.py
@@ -1,5 +1,6 @@
+import asyncio
 import datetime
-from typing import TypeVar, Type
+from typing import TypeVar, Type, Generic, Coroutine
 
 T = TypeVar('T')
 
@@ -21,3 +22,23 @@ class Singleton:
     def set_this(self: T) -> T:
         self.set(self)
         return self
+
+
+class AsyncPool(Generic[T]):
+    def __init__(self, concurrency_limit: int):
+        self.semaphore = asyncio.Semaphore(concurrency_limit)
+        self.tasks: list[Coroutine] = list()
+        self.results: list[T] = list()
+        self.stop = False
+
+    def add_task(self, coro: Coroutine):
+        self.tasks.append(coro)
+
+    async def run(self):
+        self.results = [None] * len(self.tasks)
+        await asyncio.gather(*[self._run_one(i, coro) for i, coro in enumerate(self.tasks)])
+
+    async def _run_one(self, idx: int, coro: Coroutine):
+        async with self.semaphore:
+            if not self.stop:
+                self.results[idx] = await coro
